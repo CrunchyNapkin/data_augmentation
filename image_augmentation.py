f8cd1ce7ac16599f850108520f9c5f07f51da879
Eric B <e.b.brouwer@hotmail.com>
2022-04-08 17:00:33 +0200

    repository created

--- image_augmentation.py
+++ image_augmentation.py
@@ -37,3 +37,31 @@ def get_random_pixel_drop(drop_num, l_bound = 0, u_bound = 255):
             input_img[rand_x, rand_y, :]= c
         return input_img
     return drop
+
+
+    # Not my code -- 
+def gaussian_blur(image, kernel_size=23, padding='SAME'):
+    sigma = tf.random.uniform((1,))* 1.9 + 0.1
+
+    radius = tf.cast(kernel_size / 2, tf.int32)
+    kernel_size = radius * 2 + 1
+    x = tf.cast(tf.range(-radius, radius + 1), tf.float32)
+    blur_filter = tf.exp(
+        -tf.pow(x, 2.0) / (2.0 * tf.pow(tf.cast(sigma, tf.float32), 2.0)))
+    blur_filter /= tf.reduce_sum(blur_filter)
+    # One vertical and one horizontal filter.
+    blur_v = tf.reshape(blur_filter, [kernel_size, 1, 1, 1])
+    blur_h = tf.reshape(blur_filter, [1, kernel_size, 1, 1])
+    num_channels = tf.shape(image)[-1]
+    blur_h = tf.tile(blur_h, [1, 1, num_channels, 1])
+    blur_v = tf.tile(blur_v, [1, 1, num_channels, 1])
+    expand_batch_dim = image.shape.ndims == 3
+    if expand_batch_dim:
+        image = tf.expand_dims(image, axis=0)
+    blurred = tf.nn.depthwise_conv2d(
+        image, blur_h, strides=[1, 1, 1, 1], padding=padding)
+    blurred = tf.nn.depthwise_conv2d(
+        blurred, blur_v, strides=[1, 1, 1, 1], padding=padding)
+    if expand_batch_dim:
+        blurred = tf.squeeze(blurred, axis=0)
+    return blurred
